3ebfc52ac6211a59e5da2a79b01838a709105b59
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2014-11-29 12:57:27 +0100

    fixed typo bug

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ languages = {
         'Chinese' : 'zh_CN.UTF-8'
         }
 
-months = {'January':1, 'Feburary':2, 'March':3, 'April':4, 'May':5,
+months = {'January':1, 'Februrary':2, 'March':3, 'April':4, 'May':5,
         'June':6, 'July':7, 'August':8, 'September':9, 'October':10,
         'November':11, 'December':12}
 
